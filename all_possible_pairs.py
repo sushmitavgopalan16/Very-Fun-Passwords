bdd4c528d0f9a711e0c998d789648d9b52a89bbe
Haylee Fay Ham <hayleeham@gmail.com>
2017-05-23 16:51:57 -0500

    don't save the passwords list in memory when creating the pairs

--- all_possible_pairs.py
+++ all_possible_pairs.py
@@ -7,13 +7,13 @@ def all_possible_pairs(filename):
         for line in text_file.read().split():
             passwords.append(line)
 
-    password_pairs = []
-    for i in range(len(passwords)):
-        for j in range(i+1,len(passwords)):
-            password_pairs.append((passwords[i], passwords[j]))
+        password_pairs = []
+        for i in range(len(passwords)):
+            for j in range(i+1,len(passwords)):
+                password_pairs.append((passwords[i], passwords[j]))
 
-    with open(('password_pairs.txt'), "w") as output:
-        output.write('\n'.join('%s %s' % pair for pair in password_pairs))
+        with open(('password_pairs.txt'), "w") as output:
+            output.write('\n'.join('%s %s' % pair for pair in password_pairs))
 
 if __name__ == '__main__':
     filename = sys.argv[1]
