1433a6d18d3502d4382995b0b37d96c8de490015
sushmitavgopalan16 <sushmitavgopalan@Sushmitas-MacBook-Pro.local>
2017-05-23 13:19:27 -0500

    changed dictionary_from_file slightly

--- dictionary_words.py
+++ dictionary_words.py
@@ -36,13 +36,15 @@ def add_word(word,trie):
 def is_word(word, trie):
 	#print("THIS IS WHAT IS PASSED TO IS_WORD ",type(trie))
 	rv = False
+	# base case length = 0
 	if len(word) == 0:
 		return rv
 
 	if word[0] not in trie.keys():
 		return rv
+		
 	if len(word) == 1:
-		if trie[word]['final'] == True:
+		if trie[word]['final']:
 			rv = True
 			return rv
 		else:
@@ -56,21 +58,15 @@ def is_word(word, trie):
 
 def build_dictionary(word_list):
 	dictionary = create_trie_node()
-	#print("TYPE TO CHECK ",type(dictionary))
-	#print("CALLING BUILD DICTIONARY ")
 	for word in word_list:
 		add_word(word,dictionary)
 	return dictionary
 
 def build_dictionary_from_file(filename):
-	#print("CALLING BUILD DICT FROM FILE")
-
-	listx = []
+	dictionary = create_trie_node()
 	with open(filename, "r") as file:
 		for line in file:
-			listx.append(line.split()[0].lower())
-	dictionary = build_dictionary(listx)
-	#print("PRINTING TYPE OF OUTPUT FROM BUILD DICT FROM FILE ", type(dictionary))
+			add_word((line.split()[0].lower()),dictionary)
 	return dictionary
 
 # build dictionaries
@@ -82,11 +78,9 @@ common_nouns = build_dictionary(list_common_nouns)
 # ALL DICTIONARY WORDS
 list_all_words = {x.name().split('.', 1)[0] for x in wn.all_synsets()}
 all_words = build_dictionary(list_all_words)
-#print("PRINTING TYPE: ", type(all_words))
 
 # LAST NAMES
 last_names_dict = build_dictionary_from_file("last_names.txt")
-#print("THIS IS WHAT IT LOOKS LIKE AFTER IT IS INITIALIZED", type(last_names_dict))
 #
 # FEMALE NAMES
 female_names_dict = build_dictionary_from_file("female_first_names.txt")
@@ -96,25 +90,20 @@ male_names_dict = build_dictionary_from_file("male_first_names.txt")
 
 def dictionary_word(word):
 	global all_words
-	#print(type(all_words))
 	return is_word(word,all_words)
 
 def common_noun(word):
 	global common_nouns
-	#print(type(common_nouns))
 	return is_word(word,common_nouns)
 
 def last_names(word):
 	global last_names_dict
-	#print("THIS IS WHAT IT LOOKS LIKE BEFORE PASSED TO IS_WORDS " ,type(last_names_dict))
 	return is_word(word,last_names_dict)
 #
 def female_names(word):
 	global female_names_dict
-	#print(type(female_names))
 	return is_word(word,female_names_dict)
 
 def male_names(word):
 	global male_names_dict
-	#print(type(all_words))
 	return is_word(word,male_names_dict)
