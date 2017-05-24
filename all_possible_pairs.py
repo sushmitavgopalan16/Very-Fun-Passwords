435ea05816d7ceb8fbada8b64824e192aa8175b3
Haylee Fay Ham <hayleeham@gmail.com>
2017-05-24 18:00:34 -0500

    less memory for pairs

--- all_possible_pairs.py
+++ all_possible_pairs.py
@@ -1,16 +1,15 @@
 import sys
 
 def all_possible_pairs(filename):
-    passwords = []
 
-    with open(filename, "r") as text_file:
-        for line in text_file.read().split():
-            passwords.append(line)
-
-            with open(('password_pairs.txt'), "w") as output:
-                for i in range(len(passwords)):
-                    for j in range(i+1,len(passwords)):
-                        pair = passwords[i] + ' ' + passwords[j] + '\n'
+    with open(('password_pairs.txt'), "w") as output:
+        with open(filename, "r") as first_file:
+            for first in first_file.readlines():
+                with open(filename, "r") as second_file:
+                    for second in second_file.readlines():
+                        first = first.strip()
+                        second = second.strip()
+                        pair = first + ' ' + second + '\n'
                         output.write(pair)
 
 if __name__ == '__main__':
