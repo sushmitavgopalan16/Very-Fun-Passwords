d7733ad59327b387aa323d6f7ea9c928271916b6
Haylee Fay Ham <hayleeham@gmail.com>
2017-05-24 17:19:51 -0500

    making pairs not all in memory

--- all_possible_pairs.py
+++ all_possible_pairs.py
@@ -7,13 +7,11 @@ def all_possible_pairs(filename):
         for line in text_file.read().split():
             passwords.append(line)
 
-        password_pairs = []
-        for i in range(len(passwords)):
-            for j in range(i+1,len(passwords)):
-                password_pairs.append((passwords[i], passwords[j]))
-
         with open(('password_pairs.txt'), "w") as output:
-            output.write('\n'.join('%s %s' % pair for pair in password_pairs))
+            for i in range(len(passwords)):
+                for j in range(i+1,len(passwords)):
+                    pair = passwords[i] + ' ' + passwords[j] + '\n'
+                    output.write(pair)
 
 if __name__ == '__main__':
     filename = sys.argv[1]
