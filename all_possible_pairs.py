100fd17a04efec5680b0d18b1ece57560b8151d6
Haylee Fay Ham <hayleeham@gmail.com>
2017-05-24 17:26:16 -0500

    indenting all under the for loop in pairs

--- all_possible_pairs.py
+++ all_possible_pairs.py
@@ -7,11 +7,11 @@ def all_possible_pairs(filename):
         for line in text_file.read().split():
             passwords.append(line)
 
-        with open(('password_pairs.txt'), "w") as output:
-            for i in range(len(passwords)):
-                for j in range(i+1,len(passwords)):
-                    pair = passwords[i] + ' ' + passwords[j] + '\n'
-                    output.write(pair)
+            with open(('password_pairs.txt'), "w") as output:
+                for i in range(len(passwords)):
+                    for j in range(i+1,len(passwords)):
+                        pair = passwords[i] + ' ' + passwords[j] + '\n'
+                        output.write(pair)
 
 if __name__ == '__main__':
     filename = sys.argv[1]
