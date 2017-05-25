a02bc79348b919c4214306ef3298b8f0652c440f
Haylee Fay Ham <hayleeham@gmail.com>
2017-05-24 21:40:11 -0500

    don't double up on pairs

--- all_possible_pairs.py
+++ all_possible_pairs.py
@@ -4,13 +4,18 @@ def all_possible_pairs(filename):
 
     with open(('password_pairs.txt'), "w") as output:
         with open(filename, "r") as first_file:
+            i1 = 0
             for first in first_file:
                 with open(filename, "r") as second_file:
+                    i2 = 0
                     for second in second_file:
                         first = first.strip()
                         second = second.strip()
-                        pair = first + ' ' + second + '\n'
-                        output.write(pair)
+                        if i2 > i1:
+                            pair = first + ' ' + second + '\n'
+                            output.write(pair)
+                        i2 += 1
+                i1 += 1
 
 if __name__ == '__main__':
     filename = sys.argv[1]
