9b6f22e30958cc5e4d4f3bcc9a763ebb6f2421f8
sushmitavgopalan16 <sushmitavgopalan@Sushmitas-MacBook-Pro.local>
2017-06-06 09:31:00 -0500

    new pw lengths

---
+++ newstats/mr_pwlength.py
@@ -0,0 +1,26 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.protocol import JSONValueProtocol
+import itertools
+
+# note - this gets the distribution for the PASSWORDS, not subsequences
+class MRTaskLengthDistribution(MRJob):
+    INPUT_PROTOCOL = JSONValueProtocol
+
+    def mapper(self, _, dictionary):
+        # don't count duplicates
+        k = dictionary['password']
+        yield str(len(k)),1
+        #yield len(password[0]),1
+
+    def combiner(self,name,counts):
+        sum_counts = sum(counts)
+        yield name, sum_counts
+
+    def reducer(self, name, counts):
+        sum_counts = sum(counts)
+        yield name, sum_counts
+
+
+if __name__ == '__main__':
+    MRTaskLengthDistribution.run()
