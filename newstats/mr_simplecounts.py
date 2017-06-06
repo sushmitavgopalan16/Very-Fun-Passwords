39fa3f06a07abc1296e481d87429e8bb6bdad3da
sushmitavgopalan16 <sushmitavgopalan@Sushmitas-MacBook-Pro.local>
2017-06-06 10:17:50 -0500

    simple counts mr

---
+++ newstats/mr_simplecounts.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.protocol import JSONValueProtocol
+import itertools
+
+class MRTask1(MRJob):
+
+    INPUT_PROTOCOL = JSONValueProtocol
+
+    def mapper(self, _, dictionary):
+        for pattern_list in dictionary['pattern']:
+            yield pattern_list[1],1
+
+    def combiner(self,name,counts):
+        sum_counts = sum(counts)
+        yield name, sum_counts
+
+    def reducer(self, name, counts):
+        sum_counts = sum(counts)
+        yield name, sum_counts
+if __name__ == '__main__':
+    MRTask1.run()
