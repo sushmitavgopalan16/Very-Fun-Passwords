f54324ffdd9e887b01033349154cd3c35eabbad2
Haylee Fay Ham <hayleeham@gmail.com>
2017-06-06 14:21:10 -0500

    adding doc strings

--- utils/find_punc.py
+++ utils/find_punc.py
@@ -1,6 +1,11 @@
 import string
 
 def is_punct(subsequence):
+	'''
+	Iterate through a password and determine whether it is all punctuation.
+	Input- subsequence: string
+	Output- punct: bool indicating whether all punctuation or not
+	'''
 	punct = True
 	for char in subsequence:
 		if char not in string.punctuation:
