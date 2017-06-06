cdb805acfaa674f5a4971cd02f1cd953f3a11825
sushmitavgopalan16 <sushmitavgopalan@uchicago.edu>
2017-06-06 16:02:02 -0500

    Update mr_least_common_subsequences.py

--- stats/mr_least_common_subsequences.py
+++ stats/mr_least_common_subsequences.py
@@ -9,6 +9,13 @@ class MRCommonSubsequences(MRJob):
   INPUT_PROTOCOL = JSONValueProtocol
 
   def mapper(self, _, dictionary):
+      '''
+      reads in json objects from the output file and digs into the
+      list of lists in the 'pattern' key to yield the subsequence
+      along with a count of -1. I use the count of -1 in order to be 
+      able to use the Priority Queue structure which is ideal to find
+      the top N most frequent objects, to find the top N LEAST frequent objects.
+		  '''
       #{"password": "14081408", "pattern": [["140", "numbers"], ["814", "date"], ["08", "numbers"]]}
       for pattern_list in dictionary['pattern']:
           yield pattern_list[0],-1
@@ -18,6 +25,10 @@ class MRCommonSubsequences(MRJob):
       yield name, sum_counts
 
   def reducer_init(self):
+     '''
+     generates a priority queue to track the top 100
+     '''
+    
      self.queue = PriorityQueue(maxsize=100)
 
   def reducer(self, name, counts):
